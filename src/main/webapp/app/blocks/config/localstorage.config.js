c42ce767aad5fed8e6639bc572d0e2d3735f0f67
unknown <ramazan_firin@hotmail.com>
2020-04-17 22:38:04 +0300

    Initial application generated by JHipster-4.13.0

---
+++ src/main/webapp/app/blocks/config/localstorage.config.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('terminalApp')
+        .config(localStorageConfig);
+
+    localStorageConfig.$inject = ['$localStorageProvider', '$sessionStorageProvider'];
+
+    function localStorageConfig($localStorageProvider, $sessionStorageProvider) {
+        $localStorageProvider.setKeyPrefix('jhi-');
+        $sessionStorageProvider.setKeyPrefix('jhi-');
+    }
+})();
